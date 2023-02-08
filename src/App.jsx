20485a8b3dbe61103319766777cad333911a2ed2
jarji <100996174+jarjia@users.noreply.github.com>
2023-02-08 17:31:24 +0400

    Modified App Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {useEffect} from 'react'
+import { BrowserRouter as Router, Routes, Route, json } from 'react-router-dom';
 import HomePage from './pages/homePage/HomePage';
 import './App.css'
 import FormAboutFile from './pages/forms/formAbout/FormAboutFile';
@@ -6,7 +7,9 @@ import { useState } from 'react';
 import FormExperienceFile from './pages/forms/formExperience/FormExperienceFile'
 
 function App() {
-  const [form, setForm] = useState()
+  const [form, setForm] = useState(null)
+
+  const HAS_ACCESS_TO_SECOND_FORM = form !== null && form.hasOwnProperty('firstName')
 
   const handleData = (data) => {
     setForm(prev => {
@@ -14,14 +17,26 @@ function App() {
     })
   }
 
-  console.log(form);
+  useEffect(() => {
+    const savedForm = JSON.parse(localStorage.getItem('react-form-app'))
+    if(savedForm) {
+      setForm(savedForm)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('react-form-app', JSON.stringify(form))
+  }, [form])
+
+  console.log('final form: ', form);
 
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomePage />}/>
-        <Route path='/formAbout' element={<FormAboutFile handleData={handleData}/>}/>
-        <Route path='/formExperience' element={<FormExperienceFile />}/>
+        <Route path='/formAbout' element={<FormAboutFile handleData={handleData}/>} />
+        {HAS_ACCESS_TO_SECOND_FORM && <Route path='/formExperience' element={<FormExperienceFile form={form} handleData={handleData}/>}/>}
+        <Route path='/me' element={<h1>me</h1>}/>
       </Routes>
     </Router>
   );
