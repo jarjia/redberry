1e144d3355fc4e17bdffb07ec4a23fc611a36d2b
jarji <100996174+jarjia@users.noreply.github.com>
2023-02-13 12:12:04 +0400

    Added 404 Page to App

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ErrorImage from '../assets/home/404-image.jpg'
+
+const ErrorPage = () => {
+  return (
+    <div className='error-page'>
+        <img src={ErrorImage} alt='error image'/>
+        <h1>Ooops... Something went wrong!<br></br> 404 page</h1>
+        <h2>Seems like you tried to access page that doesn't exist...</h2>
+        <h3><Link to='/'>Go back to home page</Link></h3>
+    </div>
+  )
+}
+
+export default ErrorPage
